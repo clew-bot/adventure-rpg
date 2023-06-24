3de3fc096740c03eb9fb1187b9c56cb1f09e2bf1
Chad Lew <lewchad11@gmail.com>
2023-06-23 18:03:42 -0700

    Adding or Port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService); // Get the ConfigService
-  await app.listen(configService.get<number>('PORT')); //
+  await app.listen(configService.get<number>('PORT') || 3000); //
 }
 
 console.log('Port listening on:', process.env.PORT);
