cdd5dfe9f64edf8ab6abe2d2f9c1af2f38abe4e7
Chad Lew <lewchad11@gmail.com>
2023-06-24 21:36:16 -0700

    Hashing passwords with bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,13 +12,17 @@ export class AuthService {
   ) {}
 
   async register(email: string, password: string): Promise<User> {
-    const user = await this.userService.createUser(email, password);
+    if (await this.userService.getUserByEmail(email)) {
+      throw new UnauthorizedException('User already exists');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userService.createUser(email, hashedPassword);
     return user;
   }
 
   async login(email: string, password: string) {
     const user = await this.userService.getUserByEmail(email);
-    if (user && user.password === password) {
+    if (user && (await bcrypt.compare(password, user.password))) {
       const payload = { username: user.email, id: user.id };
       console.log('payload: ', payload);
       return {
