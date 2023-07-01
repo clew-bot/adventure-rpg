13df589c7aabdd6acae73ccb0d16107aaf30bf17
Chad Lew <lewchad11@gmail.com>
2023-06-30 21:47:53 -0700

    Add json for levels and tree and level func

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-
+import levels from "../json/levels.json"
+import trees from "../json/trees.json"
 @Injectable()
 export class GameService {
   constructor(private readonly prisma: PrismaService) {}
   async initGame(id: any) {
-    console.log('id: ', id);
     const updateGameStarted = await this.prisma.user.update({
       where: {
         id: id,
@@ -18,6 +18,15 @@ export class GameService {
   }
 
   async getStats(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  }
+
+  async chop(userId: string) {
     const user = await this.prisma.user.findUnique({
       where: {
         id: userId,
@@ -39,5 +48,47 @@ export class GameService {
       },
     });
     return user.id;
+  }
+
+  async gainExperience(user: any, treeName: string) {
+    const tree = trees.find((tree: any) => tree.name === treeName);
+
+    if (!tree) {
+      throw new Error('Tree not found');
+    }
+
+    if (user.woodcuttingLevel < tree.level) {
+      throw new Error('Your woodcutting level is too low for this tree');
+    }
+
+    // Increase the user's woodcutting experience
+    let newExperience = user.woodcuttingExperience + tree.experience;
+    const currentLevel = levels.find((level: any) => level.level === user.woodcuttingLevel);
+    const nextLevel = levels.find((level: any) => level.level === user.woodcuttingLevel + 1);
+    );
+
+    if (!nextLevel) {
+      throw new Error('Maximum level reached');
+    }
+
+    // Check if the new experience is enough for the next level
+    if (newExperience >= nextLevel.experience) {
+      // Level up and carry over the leftover experience
+      user.woodcuttingLevel += 1;
+      newExperience -= nextLevel.experience;
+    }
+
+    user.woodcuttingExperience = newExperience;
+
+    // Update the user in the database
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        woodcuttingExperience: user.woodcuttingExperience,
+        woodcuttingLevel: user.woodcuttingLevel,
+      },
+    });
+
+    return user;
   }
 }
